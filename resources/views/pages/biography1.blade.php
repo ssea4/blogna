42fdf5876fa93b4bfa06b594ba7e7ced61f496dd
ssea4 <70467470+ssea4@users.noreply.github.com>
2021-10-12 22:04:59 +0200

    new change

--- resources/views/pages/biography1.blade.php
+++ resources/views/pages/biography1.blade.php
@@ -23,7 +23,7 @@
  
     <div class="col-lg-6">
             <div class="card mb-3">
-                <a href="#!"><img style="width:100%; height:340px" src="images/david2.jpg"></a>
+                <a href="#!"><img style="width:100%; height:340px" src="images/DAVID2.jpg"></a>
              
             </div>
         </div>
